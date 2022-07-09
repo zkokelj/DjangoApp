7d73ad503cc7d13e5cf30ab125a9776a50bf7bef
Ziga Kokelj <ziga.kokelj@gmail.com>
2022-07-09 11:06:32 +0200

    Add organization model

--- organizations/models.py
+++ organizations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class Organizations(models.Model):
+    name = models.CharField(max_length=200)
+    # TODO: :ASK: Should address have multiple fileds (street, city, country)?
+    address = models.CharField(max_length=200)
+    tax_number = models.CharField(
+        max_length=8, validators=[RegexValidator(r"^[0-9]{8}$")]
+    )
