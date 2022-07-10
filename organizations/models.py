e4ab950af5bdf26c8025b1a477f884f99084a530
Ziga Kokelj <ziga.kokelj@gmail.com>
2022-07-10 11:14:18 +0200

    Change model name, basic templates

--- organizations/models.py
+++ organizations/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from django.core.validators import RegexValidator
 
 
-class Organizations(models.Model):
+class Organization(models.Model):
     name = models.CharField(max_length=200)
     # TODO: :ASK: Should address have multiple fileds (street, city, country)?
     address = models.CharField(max_length=200)
     tax_number = models.CharField(
         max_length=8, validators=[RegexValidator(r"^[0-9]{8}$")]
     )
+
+    def __str__(self):
+        return f"{self.name} - {self.tax_number}"
--- organizations/views.py
+++ organizations/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from organizations.models import Organization
+
 
 def index(request):
-    return HttpResponse("Organizations...")
+    organizations = Organization.objects.order_by("-id")[:5]
+    output = ", ".join([o.name for o in organizations])
+    context = {"organizations": organizations}
+    return render(request, "organizations/index.html", context)
